4ed7501421ec1d38d76511eb9c9f6d47fc984fe2
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-01-17 18:12:36 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+function moveDodgerLeft() {
+        const leftNumbers = dodger.style.left.replace('px', '')
+        const left = parseInt(leftNumbers, 10);
+
+        dodger.style.left = `${left -1}px`;
+}
+
+function moveDodgerRight() {
+            const leftNumbers = dodger.style.left.replace('px', '')
+            const left = parseInt(leftNumbers, 10);
+    
+            dodger.style.left = `${left +1}px`;
+}
+
+document.addEventListener('keydown', (e) =>{
+    if(e.key === "ArrowLeft"){
+        moveDodgerLeft()
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
